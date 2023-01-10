7a632a4e594dce63faadc108dd77416ba3a69b46
GuillermoBPY <guillermo-benitez@outlook.com>
2023-01-10 10:38:05 -0300

    Actualizado

--- src/js/main.js
+++ src/js/main.js
@@ -35,42 +35,36 @@ const data = [
 ];
 let items = [...data];
 let cartObj = {};
-///----------------------PRODUCTS GRID ----------------------------------------------
+////---------------------------------------PRODUCTS HOME / GRID---------------------------------------
 
+//-----Funcionalidad Boton Add to Cart del Home-----
+
+const btnAddToCart = document.querySelector(".btnaddtocart");
+
+btnAddToCart.addEventListener("click", function (e) {
+  const selectProduct = items.find((item) => item.id == e.target.id);
+  addproduct (selectProduct)
+  printProductsInCart();
+});
 const productsGridModels = document.querySelector(".productsgrid__models");
 const productsGridCards = document.querySelector(".productsgrid__cards");
 
-//Imprime productos por Tipo Seleccionado
+
+//-----Imprime productos en la grid segun el modelo-----
 
 productsGridModels.addEventListener("click", function (e) {
-  let html = "";
   let target = e.target.parentElement.id;
-
-  if (
-    items.some((e) => {
-      return e.name === target;
-    })
-  ) {
+  if (items.some((e) => {return e.name === target;})) {
     let i = items.filter((e) => e.name === target);
-    html += ` 
-            <div class="productsgrid__card">
-                <figure class="card__figure">
-                  <img src=${i[0].image} alt=${i[0].alt}>
-                  <span id="${i[0].id}" class="card__addcircle"><i class='bx bx-plus'></i></span>
-                </figure>
-                <div class="card__info">
-                  <p>$${i[0].price} <span>| Stock: ${i[0].quantity}</span></p>
-                  <p>${i[0].name}</p>
-                </div>
-            </div>`;
-    productsGridCards.innerHTML = html;
+    printGrid(i)
   } else if (target === "Showall") {
-    printGrid();
+    printGrid(items);
   }
 });
 
-// Imprime Todos los Productos en la Products Grid
-function printGrid() {
+//-----Function para Imprimir Productos en la Grid-----
+
+function printGrid(items) {
   let html = "";
   for (let i of items) {
     html += ` 
@@ -88,214 +82,11 @@ function printGrid() {
   productsGridCards.innerHTML = html;
 }
 
-///----------------------CART----------------------------------------------
+////---------------------------------------PRODUCTS IN CART---------------------------------------
 
-const addButton = document.querySelectorAll(".bx-plus");
+//-----Function Suma cantidad en el carrito-----
 
-//---Crear Objeto Cart con los producctos seleccionados------
-
-productsGridCards.addEventListener("click", function (e) {
-  if (e.target.classList.contains("bx-plus")) {
-    let idParent = e.target.parentElement.id;
-    let selectProduct = items.find((item) => item.id == idParent);
-    if (selectProduct.quantity === 0) {
-      return alert("Cantidad no disponible!");
-    }
-    if (cartObj[idParent]) {
-      if (selectProduct.quantity === cartObj[idParent].amount) {
-        alert("Cantidad no disponible!");
-      } else {
-        cartObj[idParent].amount++;
-      }
-    } else {
-      cartObj[idParent] = {
-        ...selectProduct,
-        amount: 1,
-      };
-    }
-    console.log(idParent);
-    console.log(selectProduct);
-    console.log(items);
-    console.log(cartObj);
-    printProductsInCart();
-    printTotalCart();
-    printAmountValue();
-  }
-});
-
-//--------Imprime el Carrito
-
-const myCartBody = document.querySelector(".mycart__body");
-
-function printProductsInCart() {
-  let html = "";
-
-  const cartActual = Object.values(cartObj);
-
-  cartActual.forEach(function ({
-    id,
-    name,
-    price,
-    image,
-    alt,
-    quantity,
-    amount,
-  }) {
-    html += `
-              <div class="mycart__body--prod">
-                <figure class="mycart__body--prod--fig">
-                  <img src=${image} alt=${alt}>
-                </figure>
-                <div class="mycart__body--prod--info">
-                  <h3>${name}</h3>
-                  <p>Stock: ${quantity} <span>| $${price}</span></p>
-        <p>Subtotal: <span>$${price * amount}</span></p>
-        <p><span  id=${id} class="spanbx-minus"><i class='bx bx-minus'></i></span> ${amount} units<span id=${id} class="spanbx-plus"><i
-              class='bx bx-plus'></i></span><span id=${id} class="spanbx-trash"><i class='bx bx-trash'></i></span></p>
-      </div>
-    </div>
-      `;
-  });
-
-  myCartBody.innerHTML = html;
-  printAmountValue();
-}
-
-//--------------Funciones Botones del Cart
-
-myCartBody.addEventListener("click", function (e) {
-  const idParent = e.target.parentElement.id;
-  let selectProduct = items.find((item) => item.id == idParent);
-
-  if (e.target.classList.contains("bx-minus")) {
-    if (cartObj[idParent].amount === 1) {
-      const eliminar = confirm("Desea eliminar este producto?");
-      if (eliminar) delete cartObj[idParent];
-    } else {
-      cartObj[idParent].amount--;
-    }
-  }
-  if (e.target.classList.contains("bx-plus")) {
-    if (selectProduct.quantity === cartObj[idParent].amount) {
-      alert("Cantidad no disponible!");
-    } else {
-      cartObj[idParent].amount++;
-    }
-  }
-  if (e.target.classList.contains("bx-trash")) {
-    const eliminar = confirm("Desea eliminar este producto?");
-    if (eliminar) delete cartObj[idParent];
-  }
-
-  printProductsInCart();
-  printTotalCart();
-  printAmountValue();
-});
-
-//------------------------CHECKOUT
-
-const myCartCheckout = document.querySelector(".mycart__checkout");
-const amountCart = document.querySelector(".shoppingbagspan");
-
-// const amountCart = document.querySelector(".amountCart");
-
-function printAmountValue() {
-  let sum = 0;
-
-  const arrayCart = Object.values(cartObj);
-
-  if (!arrayCart.length) {
-    amountCart.style.display = "none";
-    return;
-  }
-
-  amountCart.style.display = "grid";
-
-  arrayCart.forEach(function ({ amount }) {
-    sum += amount;
-  });
-
-  amountCart.textContent = sum;
-}
-
-printAmountValue();
-function printTotalCart() {
-  const cartActual = Object.values(cartObj);
-
-  if (!cartActual.length) {
-    myCartBody.innerHTML = `
-        <figure class="mycart__body--empty" style="max-width: 400px; align-self: center;">
-          <img  src="./src/img/cartempty.png" alt="emptycart">
-        </figure> `;
-    myCartCheckout.innerHTML = `
-  <div class="mycart__checkout--info">
-    <p>0 items</p>
-    <h3>$0.00</h3>
-  </div>
-  <button class="mycart__checkout--btn"><i class='bx bxs-check-shield'></i>Chechkout</button>
-      `;
-    return;
-  }
-
-  let sum = 0;
-  let sumAmt = 0;
-
-  cartActual.forEach(function ({ amount, price }) {
-    sumAmt += amount;
-    sum += amount * price;
-  });
-
-  myCartCheckout.innerHTML = `
-  <div class="mycart__checkout--info">
-    <p>${sumAmt} items</p>
-    <h3>$${sum}</h3>
-  </div>
-  <button class="mycart__checkout--btn"><i class='bx bxs-check-shield'></i>Chechkout</button>
-      `;
-}
-
-//-------------Boton Comprar------------------
-
-const myCartCheckoutBtn = document.querySelector(".mycart__checkout--btn");
-
-myCartCheckout.addEventListener("click", function (e) {
-  if (e.target.classList.contains("mycart__checkout--btn")) {
-    const cartActual = Object.values(cartObj);
-    if (!cartActual.length) return alert("Su carrito esta vacio!");
-    const res = confirm("Desea realizar la compra?");
-
-    if (!res) return;
-
-    let newArray = [];
-
-    items.forEach(function (item) {
-      if (item.id === cartObj[item.id]?.id) {
-        newArray.push({
-          ...item,
-          quantity: item.quantity - cartObj[item.id].amount,
-        });
-      } else {
-        newArray.push(item);
-      }
-    });
-
-    items = newArray;
-    cartObj = {};
-  }
-
-  alert("Gracias por su compra!");
-  printGrid();
-  printProductsInCart();
-  printTotalCart();
-  printAmountValue();
-});
-
-//-------------Boton Home --------------
-
-const btnAddToCart = document.querySelector(".btnaddtocart");
-
-btnAddToCart.addEventListener("click", function (e) {
-  const selectProduct = items.find((item) => item.id == e.target.id);
+function addproduct (selectProduct){
   if (selectProduct.quantity === 0) {
     return alert("Cantidad no disponible!");
   }
@@ -311,15 +102,150 @@ btnAddToCart.addEventListener("click", function (e) {
       amount: 1,
     };
   }
+}
 
-  printProductsInCart();
-  printTotalCart();
-  printAmountValue();
+//-----Crear objeto cart con los productos seleccionados-----
+
+productsGridCards.addEventListener("click", function (e) {
+  if (e.target.classList.contains("bx-plus")) {
+    let idParent = e.target.parentElement.id;
+    let selectProduct = items.find((item) => item.id == idParent);
+    addproduct(selectProduct)
+    printProductsInCart();
+
+  }
 });
 
-printGrid();
+//-----Visualidad cantidad de items en el SHOPPBAGICON/SPAN-----
+const amountCart = document.querySelector(".shoppingbagspan");
+function printAmountValue() {
+  let sum = 0;
+  const arrayCart = Object.values(cartObj);
+  if (!arrayCart.length) {
+    amountCart.style.display = "none";
+    return;
+  }
+  amountCart.style.display = "grid";
+  arrayCart.forEach(function ({ amount }) {
+    sum += amount;
+  });
+  amountCart.textContent = sum;
+}
+
+//-----Imprime en el carrito los productos en el objeto-----
+
+const myCartBody = document.querySelector(".mycart__body");
+
+function printProductsInCart() {
+  let html = "";
+  const cartActual = Object.values(cartObj);
+  cartActual.forEach(function ({id,name,price,image,alt,quantity,amount}) {
+    html += `
+              <div class="mycart__body--prod">
+                <figure class="mycart__body--prod--fig">
+                  <img src=${image} alt=${alt}>
+                </figure>
+                <div class="mycart__body--prod--info">
+                  <h3>${name}</h3>
+                  <p>Stock: ${quantity} <span>| $${price}</span></p>
+        <p>Subtotal: <span>$${price * amount}</span></p>
+        <p><span  id=${id} class="spanbx-minus"><i class='bx bx-minus'></i></span> ${amount} units<span id=${id} class="spanbx-plus"><i
+              class='bx bx-plus'></i></span><span id=${id} class="spanbx-trash"><i class='bx bx-trash'></i></span></p>
+      </div>
+    </div>
+    `;
+  });
+  myCartBody.innerHTML = html;
+  printAmountValue();
+  printTotalCart();
+}
+
+//-----Funcionalidad de los botones del Cart-----
+
+myCartBody.addEventListener("click", function (e) {
+  const idParent = e.target.parentElement.id;
+  let selectProduct = items.find((item) => item.id == idParent);
+
+  if (e.target.classList.contains("bx-minus")) {
+  if (cartObj[idParent].amount === 1) {
+    const eliminar = confirm("Desea eliminar este producto?");
+    if (eliminar) delete cartObj[idParent];
+  } else {
+    cartObj[idParent].amount--;
+  }
+  }
+  if (e.target.classList.contains("bx-plus")) {
+    addproduct (selectProduct)
+  }
+  if (e.target.classList.contains("bx-trash")) {
+    const eliminar = confirm("Desea eliminar este producto?");
+    if (eliminar) delete cartObj[idParent];
+  }
+  printProductsInCart();
+});
+
+//------------------------CHECKOUT------------------------
+
+const myCartCheckout = document.querySelector(".mycart__checkout");
+
+//-----Function Suma cantidad en el carrito-----
+
+function printTotalCart() {
+  const cartActual = Object.values(cartObj);
+
+  if (!cartActual.length) {
+    myCartBody.innerHTML = `
+        <figure class="mycart__body--empty" style="max-width: 400px; align-self: center;">
+          <img  src="./src/img/cartempty.png" alt="emptycart">
+        </figure> `;
+  }
+  let sum = 0;
+  let sumAmt = 0;
+  cartActual.forEach(function ({ amount, price }) {
+    sumAmt += amount;
+    sum += amount * price;
+  });
+
+  myCartCheckout.innerHTML = `
+  <div class="mycart__checkout--info">
+    <p>${sumAmt} items</p>
+    <h3>$${sum}</h3>
+  </div>
+  <button class="mycart__checkout--btn"><i class='bx bxs-check-shield'></i>Chechkout</button>
+      `;
+}
+
+//-----Funcionalidad Boton Check Out del Cart-----
+
+myCartCheckout.addEventListener("click", function (e) {
+  if (e.target.classList.contains("mycart__checkout--btn")) {
+    const cartActual = Object.values(cartObj);
+    if (!cartActual.length) return alert("Su carrito esta vacio!");
+    const res = confirm("Desea realizar la compra?");
+    if (!res) return;
+    let newArray = [];
+    items.forEach(function (item) {
+      if (item.id === cartObj[item.id]?.id) {
+        newArray.push({
+          ...item,
+          quantity: item.quantity - cartObj[item.id].amount,
+        });
+      } else {
+        newArray.push(item);
+      }
+    });
+    items = newArray;
+    cartObj = {};
+  }
+
+  alert("Gracias por su compra!");
+  printGrid(items);
+  printProductsInCart();
+});
+
+printGrid(items);
 printProductsInCart();
-printTotalCart();
-printAmountValue();
+
+
 
 
